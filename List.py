a303314fc981e3d2537c840319969cef9afe2107
livinpeterson <105543047+livinpeterson@users.noreply.github.com>
2023-06-01 09:11:50 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,8 @@
+#write a program that prints out all the elements of the list that are less than 5.
+#Take this list
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for list in a:
+    if list < 5:
+        print(list)
